fbcb01a3fbfe5105d935c81b977fbbea52b31c3e
Vladimir Dorogov <i@d-vantage.com>
2018-04-13 17:53:56 +0300

    Update config.php

--- src/Resources/config.php
+++ src/Resources/config.php
@@ -1,13 +1,16 @@
 <?php
 
 return [
-    'namespace'       => 'App\Models',
-    'base_class_name' => \Illuminate\Database\Eloquent\Model::class,
-    'output_path'     => 'Models',
-    'no_timestamps'   => null,
-    'date_format'     => null,
-    'connection'      => null,
-    'db_types'        => [
+    'namespace'           => 'App\Models',
+    'base_class_name'     => \Illuminate\Database\Eloquent\Model::class,
+    'sofa_eloquence_name' => \Sofa\Eloquence\Eloquence::class,
+    'sofa_mappable_name'  => \Sofa\Eloquence\Mappable::class,
+    'sofa_mutable_name'   => \Sofa\Eloquence\Mutable::class,
+    'output_path'         => 'Models',
+    'no_timestamps'       => null,
+    'date_format'         => null,
+    'connection'          => null,
+    'db_types'            => [
         'enum' => 'string',
         'json' => 'string'
     ]
